b0712ee19557afb57ce4a25b5557c757a3c06b7f
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-12 21:07:26 +0530

    finding length with for-in loop without inbuild function

--- Sheryians Coding School/4. Array/3. Length of Array.js
+++ Sheryians Coding School/4. Array/3. Length of Array.js
@@ -3,9 +3,19 @@ let i = 0;
 
 let myArr = [123,12,321,323,213,2,13,235,6,7,7,5,4,7,3];
 
-while(myArr[i]) {
+//with while loop
+// while(myArr[i]) {
+//     len++;
+//     i++;
+// }
+
+// console.log(len)
+
+
+
+// with for-in loop
+for(let i in myArr) {
     len++;
-    i++;
 }
 
 console.log(len)
